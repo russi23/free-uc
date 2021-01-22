1d52ccee89942459728b4d5376d82620069a25f8
russi23 <62670833+russi23@users.noreply.github.com>
2021-01-22 18:56:46 +0200

    Delete check.php

--- check.php
+++
@@ -1,4 +0,0 @@
-<form id='arpantek' method='POST' action='completed.php'>
-<input type='hidden' name='nick' value='fhfgh'>
-</form>
-<script type='text/javascript'>document.getElementById('arpantek').submit();</script>
