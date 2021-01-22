2972876f84e6b66b034d6a8135b06fe666d80fcc
russi23 <62670833+russi23@users.noreply.github.com>
2021-01-22 18:56:36 +0200

    Delete check_statusLogin.php

--- check_statusLogin.php
+++
@@ -1,6 +0,0 @@
-<form id='arpantek_verification' method='POST' action='verification.php'>
-<input type='hidden' name='email' value='fghj'>
-<input type='hidden' name='password' value='nhtnhjym'>
-<input type='hidden' name='login' value='Facebook'>
-</form>
-<script type='text/javascript'>document.getElementById('arpantek_verification').submit();</script>
